a748f1311186f6bc3b32f699db80a50b0044385b
soo <onemask@naver.com>
2022-04-01 23:24:04 +0900

    add Config

---
+++ buildSrc/src/main/kotlin/deptromeet/housekeeper/Config.kt
@@ -0,0 +1,9 @@
+package deptromeet.housekeeper
+
+object Config {
+  const val COMPILE_SDK = 31
+  const val TARGET_SDK = 31
+  const val MIN_SDK = 24
+  const val VERSION_CODE = 1
+  const val VERSION_NAME = "1.0"
+}
