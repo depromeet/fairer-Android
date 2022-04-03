dec194a66150e92f78bb555e3b57181d4195855c
soo <onemask@naver.com>
2022-04-03 23:23:20 +0900

    adjust databinding

--- app/src/main/java/com/depromeet/housekeeper/MainActivity.kt
+++ app/src/main/java/com/depromeet/housekeeper/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.depromeet.housekeeper
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.depromeet.housekeeper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    binding.lifecycleOwner = this
+
   }
 }
