373560e4f4f084a577372d8e1c63cbf0475d313b
sujin-kk <sjoo0002@gmail.com>
2022-04-06 23:26:16 +0900

    feat: add todo page in 2 depth &  header component

--- app/src/main/java/com/depromeet/housekeeper/MainFragment.kt
+++ app/src/main/java/com/depromeet/housekeeper/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.depromeet.housekeeper.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -18,7 +19,16 @@ class MainFragment : Fragment() {
   ): View {
     binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
     binding.lifecycleOwner = this.viewLifecycleOwner
+
+    binding.mainAddTodoFb.setOnClickListener {
+      navigateToAddTodoPage()
+    }
+
     return binding.root
+  }
+
+  private fun navigateToAddTodoPage() {
+    binding.mainAddTodoFb.findNavController().navigate(R.id.action_mainFragment_to_addTodoFragment1)
   }
 
 }
