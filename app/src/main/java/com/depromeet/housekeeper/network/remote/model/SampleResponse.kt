6a72c65b4f5b18706ad23c6f561032c563e04de9
soo <onemask@naver.com>
2022-04-09 00:56:23 +0900

    add sampleData

---
+++ app/src/main/java/com/depromeet/housekeeper/network/remote/model/SampleResponse.kt
@@ -0,0 +1,12 @@
+package com.depromeet.housekeeper.network.remote.model
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class SampleResponse(
+  val breeds: List<String>?,
+  val height: Int,
+  val id: String,
+  val url: String,
+  val width: Int,
+)
