1c5cfa4eb793a0ce4dea113a235b9cf6bc2d5f99
soo <onemask@naver.com>
2022-05-03 22:18:16 +0900

    add DataClass

---
+++ app/src/main/java/com/depromeet/housekeeper/model/SpaceChores.kt
@@ -0,0 +1,10 @@
+package com.depromeet.housekeeper.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class SpaceChores(
+  var spaceName: String,
+  var houseWorks: List<String>,
+) : Parcelable
