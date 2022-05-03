659a97a489e175947606dd4dc6c54ea4c1ef4d88
soo <onemask@naver.com>
2022-05-04 01:00:32 +0900

    add data

--- app/src/main/java/com/depromeet/housekeeper/model/HouseWork.kt
+++ app/src/main/java/com/depromeet/housekeeper/model/HouseWork.kt
@@ -4,12 +4,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class HouseWork(
-    val assignees: List<Assignee>,
-    val houseWorkId: Int,
-    val houseWorkName: String,
-    val scheduledDate: String,
-    val scheduledTime: String,
-    val space: String,
-    val success: Boolean,
-    val successDateTime: String?
+  val assignees: List<Assignee>?,
+  val houseWorkId: Int,
+  val houseWorkName: String,
+  val scheduledDate: String,
+  val scheduledTime: String,
+  val space: String,
+  val success: Boolean,
+  val successDateTime: String?,
 )
