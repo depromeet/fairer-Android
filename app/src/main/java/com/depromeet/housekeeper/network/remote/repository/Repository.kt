c6289b1f8a89d39f877f9bf7d52db2e744821579
soo <onemask@naver.com>
2022-05-04 00:55:31 +0900

    change object

--- app/src/main/java/com/depromeet/housekeeper/network/remote/repository/Repository.kt
+++ app/src/main/java/com/depromeet/housekeeper/network/remote/repository/Repository.kt
@@ -1,10 +1,15 @@
 package com.depromeet.housekeeper.network.remote.repository
 
+import com.depromeet.housekeeper.model.HouseWorks
+import com.depromeet.housekeeper.network.remote.api.ApiService
 import com.depromeet.housekeeper.network.remote.api.RemoteDataSource
 import com.depromeet.housekeeper.network.remote.model.SampleResponse
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
-class Repository(private val remoteDataSource: RemoteDataSource) {
-  suspend fun getList(): Flow<List<SampleResponse>> = remoteDataSource.getList()
-
+object Repository : RemoteDataSource {
+  private val ApiService = RetrofitBuilder.apiService
+  override suspend fun getList(scheduledDate: String): Flow<List<HouseWorks>> = flow {
+    ApiService.getList(scheduledDate)
+  }
 }
