dcdde3923b63b206559cd1d98386d7b723807b26
soo <onemask@naver.com>
2022-05-04 00:55:43 +0900

    구조 바꿈

--- app/src/main/java/com/depromeet/housekeeper/network/remote/api/ApiService.kt
+++ app/src/main/java/com/depromeet/housekeeper/network/remote/api/ApiService.kt
@@ -1,11 +1,13 @@
 package com.depromeet.housekeeper.network.remote.api
 
+import com.depromeet.housekeeper.model.HouseWorks
 import com.depromeet.housekeeper.network.remote.model.SampleResponse
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface ApiService {
   //Sample API
-  @GET("/v1/images/search")
-  suspend fun getList(): List<SampleResponse>
+  @GET("/api/houseworks")
+  suspend fun getList(@Query("scheduledDate") scheduledDate: String): List<HouseWorks>
 }
