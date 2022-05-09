044b0ab644e3680c1d29a0f6351bd425c4142abf
soo <onemask@naver.com>
2022-05-09 22:53:41 +0900

    add patch api

--- app/src/main/java/com/depromeet/housekeeper/network/remote/api/ApiService.kt
+++ app/src/main/java/com/depromeet/housekeeper/network/remote/api/ApiService.kt
@@ -1,13 +1,16 @@
 package com.depromeet.housekeeper.network.remote.api
 
+import com.depromeet.housekeeper.model.ChorePreset
 import com.depromeet.housekeeper.model.Chores
-import com.depromeet.housekeeper.network.remote.model.HouseWorkCreateResponse
-import retrofit2.http.Body
 import com.depromeet.housekeeper.model.CompleteHouseWork
 import com.depromeet.housekeeper.model.HouseWorks
+import com.depromeet.housekeeper.model.UpdateChoreResponse
+import com.depromeet.housekeeper.network.remote.model.HouseWorkCreateResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
-import com.depromeet.housekeeper.model.ChorePreset
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
@@ -20,8 +23,10 @@ interface ApiService {
   @GET("/api/preset")
   suspend fun getChoreList(): ChorePreset
 
-  //Sample API
   @GET("/api/houseworks/success/count")
   suspend fun getCompletedHouseWorkNumber(@Query("scheduledDate") scheduledDate: String): CompleteHouseWork
+
+  @PATCH("/api/houseworks/{houseWorkId}")
+  suspend fun updateChoreState(@Path("houseWorkId") houseWorkId: Int): UpdateChoreResponse
 
 }
