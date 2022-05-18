7a2006cc35ad5df056a5cd2559bab1303a46328d
soo <onemask@naver.com>
2022-05-19 00:53:22 +0900

    AuthInterceptor 이동

--- app/src/main/java/com/depromeet/housekeeper/network/remote/repository/RetrofitBuilder.kt
+++ app/src/main/java/com/depromeet/housekeeper/network/remote/repository/RetrofitBuilder.kt
@@ -1,12 +1,15 @@
 package com.depromeet.housekeeper.network.remote.repository
 
+import com.depromeet.housekeeper.local.PrefsManager
 import com.depromeet.housekeeper.network.remote.api.ApiService
-import com.depromeet.housekeeper.network.remote.interceptor.AuthInterceptor
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
+import okio.IOException
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
@@ -19,12 +22,11 @@ object RetrofitBuilder {
   }
 
   private val okHttpBuilder = OkHttpClient.Builder()
-    .addInterceptor(httpLoggingInterceptor)
+    .addInterceptor(AuthInterceptor())
+    .addNetworkInterceptor(httpLoggingInterceptor)
     .connectTimeout(1, TimeUnit.SECONDS)
     .readTimeout(1, TimeUnit.SECONDS)
     .writeTimeout(1, TimeUnit.SECONDS)
-    .addInterceptor(AuthInterceptor())
-
 
   private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -32,11 +34,21 @@ object RetrofitBuilder {
 
   private val retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
-    .client(okHttpBuilder.build()
-    )
+    .client(okHttpBuilder.build())
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .build()
 
   val apiService: ApiService = retrofit.create(ApiService::class.java)
+
+  private class AuthInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
+      proceed(
+        request().newBuilder()
+          .addHeader("AUTHORIZATION", PrefsManager.accessToken)
+          .build()
+      )
+    }
+  }
 }
