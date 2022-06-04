176a135d13f0d66147ea71165dbefb8a713ff738
wjdwns <wjdwns3744@naver.com>
2022-06-05 01:02:04 +0900

    이름 작성 페이지 작업

---
+++ app/src/main/java/com/depromeet/housekeeper/ui/SignNameFragment.kt
@@ -0,0 +1,58 @@
+package com.depromeet.housekeeper.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.depromeet.housekeeper.R
+import com.depromeet.housekeeper.databinding.FragmentSignNameBinding
+import kotlinx.coroutines.flow.collect
+import timber.log.Timber
+
+class SignNameFragment : Fragment() {
+    lateinit var binding : FragmentSignNameBinding
+    private val viewmodel : SignNameViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sign_name,container,false)
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        binding.vm = viewmodel
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+        bindingvm()
+    }
+
+    private fun bindingvm() {
+        lifecycleScope.launchWhenCreated {
+            viewmodel.inputName.collect {
+                binding.signNameNextBtn.mainFooterButton.isEnabled = viewmodel.inputName.value!=""
+                Timber.d(binding.signNameEt.text.toString())
+            }
+        }
+    }
+
+    private fun initListener(){
+        binding.signNameNextBtn.mainFooterButton.setText(R.string.sign_name_next_btn_text)
+        binding.signNameEt.addTextChangedListener {
+            viewmodel.setinputname(binding.signNameEt.text.toString())
+        }
+        binding.signNameNextBtn.mainFooterButton.setOnClickListener {
+            //findNavController().navigate()
+        }
+    }
+}
---
+++ app/src/main/java/com/depromeet/housekeeper/ui/SignNameViewModel.kt
@@ -0,0 +1,15 @@
+package com.depromeet.housekeeper.ui
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class SignNameViewModel : ViewModel() {
+    private val _inputName : MutableStateFlow<String> = MutableStateFlow("")
+    val inputName : StateFlow<String>
+    get() = _inputName
+
+    fun setinputname(name:String){
+        _inputName.value = name
+    }
+}
