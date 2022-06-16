be4db622cc85d7b5d4a4ef4b38d40de97b809312
wjdwns <50354282+wjdwns@users.noreply.github.com>
2022-06-16 21:37:44 +0900

    Feature/fixtextfield (#69)
    * profile recyclerview 수정
    
    * text field figma 컴포넌트대로 수정

--- app/src/main/java/com/depromeet/housekeeper/util/BindingAdapter.kt
+++ app/src/main/java/com/depromeet/housekeeper/util/BindingAdapter.kt
@@ -1,8 +1,12 @@
 package com.depromeet.housekeeper.util
 
 import android.graphics.Typeface
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 import com.depromeet.housekeeper.R
 import com.depromeet.housekeeper.model.enums.SignViewType
 
@@ -18,14 +22,14 @@ object BindingAdapter {
   }
   @androidx.databinding.BindingAdapter("app:loadImage")
   @JvmStatic
-  fun loadImage(imageView : ImageView, resid:Int ){
-    imageView.setImageResource(resid)
-  }
-
-  @androidx.databinding.BindingAdapter("app:isSelected")
-  @JvmStatic
-  fun isSelected(imageView: ImageView, isTrue : Boolean){
-    imageView.isSelected = isTrue
+  fun loadImage(imageView : ImageView, url:String){
+    Glide.with(imageView.context)
+      .load(url)
+      .placeholder(R.drawable.bg_profile_imageview_inactive)
+      .error(R.drawable.bg_profile_imageview_inactive)
+      .diskCacheStrategy(DiskCacheStrategy.NONE)
+      .apply(RequestOptions().fitCenter())
+      .into(imageView)
   }
 
   @androidx.databinding.BindingAdapter("app:signViewType")
@@ -45,5 +49,15 @@ object BindingAdapter {
         textView.setText(R.string.modify_group_name_text)
       }
     }
+  }
+  @androidx.databinding.BindingAdapter("app:imageUrl","app:placeholder")
+  @JvmStatic fun loadImage(imageView: ImageView, url: String, placeholder: Drawable){
+    Glide.with(imageView.context)
+      .load(url)
+      .placeholder(placeholder)
+      .error(placeholder)
+      .diskCacheStrategy(DiskCacheStrategy.NONE)
+      .apply(RequestOptions().fitCenter())
+      .into(imageView)
   }
 }
