e07b98863fe4a96b87d91211e7f36623b1457b15
jeong jun <wjdwns3744@naver.com>
2022-06-17 21:04:29 +0900

    firebase dynamiclink 추가

--- app/src/main/java/com/depromeet/housekeeper/MainActivity.kt
+++ app/src/main/java/com/depromeet/housekeeper/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.depromeet.housekeeper
 
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -10,18 +11,43 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.depromeet.housekeeper.databinding.ActivityMainBinding
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.ktx.Firebase
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-  private val viewmodel : MainViewModel by viewModels()
+  private val viewModel : MainViewModel by viewModels()
   lateinit var binding: ActivityMainBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     installSplashScreen().apply {
-      setKeepOnScreenCondition{viewmodel.isLoading.value}
+      setKeepOnScreenCondition{viewModel.isLoading.value}
     }
     super.onCreate(savedInstanceState)
     binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     binding.lifecycleOwner = this
+    getDynamicLink()
+  }
+
+  private fun getDynamicLink(){
+    Firebase.dynamicLinks
+      .getDynamicLink(intent)
+      .addOnSuccessListener(this) { pendingDynamicLinkData ->
+        // Get deep link from result (may be null if no link is found)
+        var deepLink: Uri? = null
+        if (pendingDynamicLinkData != null) {
+          deepLink = pendingDynamicLinkData.link
+          val code = deepLink?.getQueryParameter("code")
+          viewModel.setInviteCode(code!!)
+          Timber.d("!@$code")
+        }
+        else{
+          Timber.d("NO HAVE DEEP LINK")
+        }
+        Timber.d("!!$deepLink")
+
+      }
+      .addOnFailureListener(this) { e -> Timber.w( "getDynamicLink:onFailure $e") }
   }
 }
