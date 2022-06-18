d39148653841ed7a34a8f0c1e2b5fc3310ba2137
sujin-kk <sjoo0002@gmail.com>
2022-06-19 00:44:34 +0900

    셋팅 아이콘 추가

---
+++ app/src/main/java/com/depromeet/housekeeper/ui/ManageHouseFragment.kt
@@ -0,0 +1,20 @@
+package com.depromeet.housekeeper.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.depromeet.housekeeper.R
+
+class ManageHouseFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_manage_house, container, false)
+    }
+
+}
