4acb083d11e8c08144807bb65a2f2d2d5da0735f
wjdwns <wjdwns3744@naver.com>
2022-06-22 15:18:39 +0900

    어노테이션 추가

--- app/src/main/java/com/depromeet/housekeeper/model/GetInviteCode.kt
+++ app/src/main/java/com/depromeet/housekeeper/model/GetInviteCode.kt
@@ -1,7 +1,11 @@
 package com.depromeet.housekeeper.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GetInviteCode(
     val inviteCode: String,
     val inviteCodeExpirationDateTime: String,
     val teamName: String
-)
+):Parcelable
