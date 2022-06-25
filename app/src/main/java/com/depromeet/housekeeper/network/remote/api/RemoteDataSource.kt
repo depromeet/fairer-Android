f451abf44543888e9d4bd5e490577ddf8247a473
wjdwns <wjdwns3744@naver.com>
2022-06-25 17:01:52 +0900

    코드 reformat

--- app/src/main/java/com/depromeet/housekeeper/network/remote/api/RemoteDataSource.kt
+++ app/src/main/java/com/depromeet/housekeeper/network/remote/api/RemoteDataSource.kt
@@ -6,33 +6,34 @@ import com.depromeet.housekeeper.network.remote.model.LoginResponse
 import kotlinx.coroutines.flow.Flow
 
 interface RemoteDataSource {
-  suspend fun createHouseWorks(houseWorks: Chores): Flow<HouseWorkCreateResponse>
-  suspend fun getList(scheduledDate: String): Flow<List<HouseWorks>>
-  suspend fun getHouseWorkList(): Flow<ChorePreset>
-  suspend fun getCompletedHouseWorkNumber(scheduledDate: String): Flow<CompleteHouseWork>
-  suspend fun getGoogleLogin(socialType: SocialType): Flow<LoginResponse>
-  suspend fun deleteHouseWork(houseWorkId: Int): Flow<Unit>
-  suspend fun editHouseWork(houseWorkId: Int, chore: Chore): Flow<HouseWork>
-  suspend fun updateChoreState(
-    houseWorkId: Int,
-    updateChoreBody: UpdateChoreBody,
-  ): Flow<UpdateChoreResponse>
-  suspend fun logout(auth: String): Flow<Unit>
-  suspend fun buildTeam(buildTeam: BuildTeam): Flow<BuildTeamResponse>
-  suspend fun getTeam(): Flow<Groups>
-  suspend fun getProfileImages(): Flow<ProfileImages>
-  suspend fun updateMember(updateMember: UpdateMember): Flow<UpdateMemberResponse>
-  suspend fun getInviteCode() : Flow<GetInviteCode>
-  suspend fun updateTeam(teamName : BuildTeam) : Flow<TeamUpdateResponse>
-  suspend fun joinTeam(inviteCode : JoinTeam) : Flow<JoinTeamResponse>
-  suspend fun createRule(rule: Rule): Flow<RuleResponses>
-  suspend fun getRules(): Flow<RuleResponses>
-  suspend fun deleteRule(ruleId: Int): Flow<Response>
-  suspend fun leaveTeam() : Flow<Unit>
-  suspend fun getMe(): Flow<ProfileData>
-  suspend fun updateMe(
-    memberName: String,
-    profilePath: String,
-    statueMessage: String,
-  ): Flow<ProfileData>
+    suspend fun createHouseWorks(houseWorks: Chores): Flow<HouseWorkCreateResponse>
+    suspend fun getList(scheduledDate: String): Flow<List<HouseWorks>>
+    suspend fun getHouseWorkList(): Flow<ChorePreset>
+    suspend fun getCompletedHouseWorkNumber(scheduledDate: String): Flow<CompleteHouseWork>
+    suspend fun getGoogleLogin(socialType: SocialType): Flow<LoginResponse>
+    suspend fun deleteHouseWork(houseWorkId: Int): Flow<Unit>
+    suspend fun editHouseWork(houseWorkId: Int, chore: Chore): Flow<HouseWork>
+    suspend fun updateChoreState(
+        houseWorkId: Int,
+        updateChoreBody: UpdateChoreBody,
+    ): Flow<UpdateChoreResponse>
+
+    suspend fun logout(auth: String): Flow<Unit>
+    suspend fun buildTeam(buildTeam: BuildTeam): Flow<BuildTeamResponse>
+    suspend fun getTeam(): Flow<Groups>
+    suspend fun getProfileImages(): Flow<ProfileImages>
+    suspend fun updateMember(updateMember: UpdateMember): Flow<UpdateMemberResponse>
+    suspend fun getInviteCode(): Flow<GetInviteCode>
+    suspend fun updateTeam(teamName: BuildTeam): Flow<TeamUpdateResponse>
+    suspend fun joinTeam(inviteCode: JoinTeam): Flow<JoinTeamResponse>
+    suspend fun createRule(rule: Rule): Flow<RuleResponses>
+    suspend fun getRules(): Flow<RuleResponses>
+    suspend fun deleteRule(ruleId: Int): Flow<Response>
+    suspend fun leaveTeam(): Flow<Unit>
+    suspend fun getMe(): Flow<ProfileData>
+    suspend fun updateMe(
+        memberName: String,
+        profilePath: String,
+        statueMessage: String,
+    ): Flow<ProfileData>
 }
