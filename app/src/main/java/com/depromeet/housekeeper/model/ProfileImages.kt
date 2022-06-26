4f6e8430933d563be27d56b97c026ea66f0e7bfd
soo <onemask@naver.com>
2022-06-26 15:48:46 +0900

    fix bug

--- app/src/main/java/com/depromeet/housekeeper/model/ProfileImages.kt
+++ app/src/main/java/com/depromeet/housekeeper/model/ProfileImages.kt
@@ -1,6 +1,5 @@
 package com.depromeet.housekeeper.model
 
 data class ProfileImages(
-    val bigImageList : List<String>,
-    val smallImageList : List<String>
+    val bigImageList : List<String>
 )
