882531b26ffc1792281fc253e4a09e1a2284434b
soo <onemask@naver.com>
2022-06-28 01:10:49 +0900

    fix build error

--- app/src/main/java/com/depromeet/housekeeper/adapter/AddAssigneeAdapter.kt
+++ app/src/main/java/com/depromeet/housekeeper/adapter/AddAssigneeAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.depromeet.housekeeper.databinding.ItemProfileAddBinding
 import com.depromeet.housekeeper.databinding.ItemProfileBinding
 import com.depromeet.housekeeper.model.Assignee
 import com.depromeet.housekeeper.model.AssigneeSelect
