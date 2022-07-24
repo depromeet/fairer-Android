ce7eb766f42b5e2d110a0206cb700801a2cd6042
wjdwns <wjdwns3744@naver.com>
2022-07-24 23:04:36 +0900

    modify retrofit server address

--- app/src/main/java/com/depromeet/housekeeper/network/remote/repository/RetrofitBuilder.kt
+++ app/src/main/java/com/depromeet/housekeeper/network/remote/repository/RetrofitBuilder.kt
@@ -5,21 +5,15 @@ import com.depromeet.housekeeper.network.remote.api.ApiService
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.Protocol
-import okhttp3.Response
-import okhttp3.ResponseBody
+import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import okio.IOException
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.DELETE
-import java.lang.Exception
 import java.util.concurrent.TimeUnit
 
 object RetrofitBuilder {
-  private const val BASE_URL = "http://ec2-13-125-232-180.ap-northeast-2.compute.amazonaws.com:8080"
+  private const val BASE_URL = "http://ec2-3-39-60-64.ap-northeast-2.compute.amazonaws.com:8080"
 
   private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
     level = HttpLoggingInterceptor.Level.BODY
