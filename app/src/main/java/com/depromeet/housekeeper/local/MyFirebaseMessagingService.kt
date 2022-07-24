bce8f18d199dfd73a86fa23d71650900d0246089
sujin-kk <sjoo0002@gmail.com>
2022-07-24 15:28:16 +0900

    [chore] local fcm setting

---
+++ app/src/main/java/com/depromeet/housekeeper/local/MyFirebaseMessagingService.kt
@@ -0,0 +1,17 @@
+package com.depromeet.housekeeper.local
+
+import android.annotation.SuppressLint
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+}
