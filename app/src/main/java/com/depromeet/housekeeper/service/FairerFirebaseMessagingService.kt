95b5848b48c70c97d3e4059445060cd1ed63889b
sujin-kk <sjoo0002@gmail.com>
2022-08-08 20:58:25 +0900

    add notification method

--- app/src/main/java/com/depromeet/housekeeper/service/FairerFirebaseMessagingService.kt
+++ app/src/main/java/com/depromeet/housekeeper/service/FairerFirebaseMessagingService.kt
@@ -1,18 +1,38 @@
 package com.depromeet.housekeeper.service
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_HIGH
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_ONE_SHOT
 import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.work.*
+import com.depromeet.housekeeper.MainActivity
+import com.depromeet.housekeeper.R
 import com.depromeet.housekeeper.local.PrefsManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import okhttp3.internal.notify
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
+import kotlin.random.Random
+
+private const val CHANNEL_NAME = "FairerNotification"
+private const val CHANNEL_DESCRIPTION = "Channel For Fairer Notification"
+private const val CHANNEL_ID = "fcm_default_channel"
+private const val TAG_FCM = "onFCM"
 
 class FairerFirebaseMessagingService: FirebaseMessagingService() {
+
     @Override
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-        Timber.d("New FCM device token : $token")
+        Timber.tag(TAG_FCM).d("New FCM device token : $token")
         PrefsManager.setDeviceToken(deviceToken = token)
     }
 
@@ -21,32 +41,52 @@ class FairerFirebaseMessagingService: FirebaseMessagingService() {
         super.onMessageReceived(message)
         Timber.d("From: ${message.from}")
         if (message.data.isNotEmpty()) {
-            Timber.d("Message data payload: ${message.data}")
+            Timber.tag(TAG_FCM).d("Message data payload: ${message.data}")
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-                scheduleJob()
-            } else {
-                // Handle message within 10 seconds
-                handleNow()
-            }
+            val title = message.data["title"]
+            val body = message.data["message"]
+
+            showNotification(messageTitle = title, messageBody = body)
         }
 
         message.notification?.let {
-            Timber.d("Message Notification Body: ${it.body}")
+            Timber.tag(TAG_FCM).d("Message Notification Body: ${it.body}")
         }
     }
 
-    private fun scheduleJob() {
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun showNotification(messageTitle: String?, messageBody: String?) {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val notificationID = Random.nextInt() // 고유 id를 지정
 
+        createNotificationChannel(notificationManager)
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+        val pendingIntent = PendingIntent.getActivity(this, notificationID, intent, FLAG_ONE_SHOT)
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_fairer)
+            .setContentTitle(messageTitle)
+            .setContentText(messageBody)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)  // 알림 터치 시 자동으로 삭제
+            .build()
+
+        notificationManager.notify(notificationID, notificationBuilder)
     }
 
-    private fun handleNow() {
-
-    }
-
-    private fun showNotification(messageTitle: String, messageBody: String) {
-        // TODO : 수신된 FCM 메세지를 포함하는 간단한 알림을 만들고 표시
+    //Oreo(26) 이상 버전에는 channel 필요, 현재 MIN_SDK = 28
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, IMPORTANCE_HIGH).apply {
+            description = CHANNEL_DESCRIPTION
+            enableLights(true)
+            lightColor = getColor(R.color.highlight)
+        }
+        notificationManager.createNotificationChannel(channel)
     }
 
     // TODO : WorkerManager 이용해서 FCM Managing
