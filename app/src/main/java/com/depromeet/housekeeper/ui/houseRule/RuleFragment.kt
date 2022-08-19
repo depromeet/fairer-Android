c8773ef7250343a0c53b8c33ba0fecdd02a7cb91
kimminju99 <mindori1019@gmail.com>
2022-08-20 07:29:04 +0900

    Fix/bug_rule_editText: 규칙 입력 버튼 완료 버튼으로 변경 & 완료하면 키보드 항상 내려가게 변경 & 10개 넘어가면 info message 빨간색 되게 변경

--- app/src/main/java/com/depromeet/housekeeper/ui/houseRule/RuleFragment.kt
+++ app/src/main/java/com/depromeet/housekeeper/ui/houseRule/RuleFragment.kt
@@ -1,109 +1,135 @@
 package com.depromeet.housekeeper.ui.houseRule
 
 import android.os.Bundle
-import android.view.KeyEvent
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import com.depromeet.housekeeper.R
-import com.depromeet.housekeeper.ui.houseRule.adapter.RuleAdapter
 import com.depromeet.housekeeper.databinding.FragmentRuleBinding
-import kotlinx.coroutines.flow.collect
+import com.depromeet.housekeeper.ui.houseRule.adapter.RuleAdapter
+import com.depromeet.housekeeper.util.EditTextUtil.hideKeyboard
+import com.depromeet.housekeeper.util.EditTextUtil.listenEditorDoneAction
+import com.depromeet.housekeeper.util.EditTextUtil.textPattern
+import timber.log.Timber
+
 
 class RuleFragment : Fragment() {
-  lateinit var binding: FragmentRuleBinding
-  private val viewModel: RuleViewModel by viewModels()
-  lateinit var adapter: RuleAdapter
+    lateinit var binding: FragmentRuleBinding
+    private val viewModel: RuleViewModel by viewModels()
+    lateinit var adapter: RuleAdapter
 
-  override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?,
-  ): View {
-    binding = DataBindingUtil.inflate(inflater, R.layout.fragment_rule, container, false)
-    binding.lifecycleOwner = this.viewLifecycleOwner
-    return binding.root
-  }
-
-  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-    super.onViewCreated(view, savedInstanceState)
-    initListener()
-    setAdapter()
-    bindingVm()
-    validateName()
-  }
-
-  private fun bindingVm() {
-    lifecycleScope.launchWhenStarted {
-      viewModel.rules.collect {
-        when {
-          it.isNotEmpty() -> {
-            binding.tvRule.visibility = View.VISIBLE
-            binding.rvRules.visibility = View.VISIBLE
-            adapter.updateDate(it.toMutableList())
-          }
-          else -> {
-            binding.tvRule.visibility = View.GONE
-            binding.rvRules.visibility = View.GONE
-          }
-        }
-      }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_rule, container, false)
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        return binding.root
     }
 
-  }
-
-  private fun setAdapter() {
-    adapter = RuleAdapter(viewModel.rules.value.toMutableList()) { viewModel.deleteRule(it) }
-    binding.rvRules.adapter = adapter
-  }
-
-  private fun initListener() {
-    binding.etRule.signNameClear.setOnClickListener {
-      binding.etRule.fairerEt.setText(R.string.sign_name_blank)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+        setAdapter()
+        bindingVm()
     }
 
-    binding.ruleHeader.apply {
-      defaultHeaderBackBtn.setOnClickListener {
-        it.findNavController().navigateUp()
-      }
-    }
-
-    binding.etRule.fairerEt.setOnKeyListener { v, keyCode, event ->
-      if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
-        val ruleName = binding.etRule.fairerEt.text
-        if (ruleName.isNotEmpty()) {
-          if(viewModel.rules.value.count()==10){
-            Toast.makeText(requireContext(),"규칙은 최대 10개까지 만들 수 있습니다.",Toast.LENGTH_LONG).show()
-            binding.etRule.fairerEt.text.clear()
-            binding.etRule.fairerEt.requestFocus()
-          }
-          else{
-            viewModel.createRule(ruleName.toString())
-            binding.etRule.fairerEt.text.clear()
-            binding.etRule.fairerEt.requestFocus()
-          }
-
+    private fun bindingVm() {
+        lifecycleScope.launchWhenStarted {
+            viewModel.rules.collect {
+                when {
+                    it.isNotEmpty() -> {
+                        binding.tvRule.visibility = View.VISIBLE
+                        binding.rvRules.visibility = View.VISIBLE
+                        adapter.updateDate(it.toMutableList())
+                        if (it.count() <10) {
+                            binding.etRule.fairerEt.isEnabled = true
+                        }
+                    }
+                    else -> {
+                        binding.tvRule.visibility = View.GONE
+                        binding.rvRules.visibility = View.GONE
+                    }
+                }
+            }
         }
 
-      }
-      false
     }
-  }
-  private fun validateName() {
-    val pattern = "[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힝|ㆍᆢ| ]*"
-    binding.etRule.fairerEt.addTextChangedListener{
-      val value: String = binding.etRule.fairerEt.text.toString()
-      binding.isTextChanged = true
-      binding.isError = !value.matches(pattern.toRegex())
-      if (value == "") {
-        binding.isTextChanged = false
-      }
+
+    private fun setAdapter() {
+        adapter = RuleAdapter(viewModel.rules.value.toMutableList()) { viewModel.deleteRule(it) }
+        binding.rvRules.adapter = adapter
     }
-  }
+
+    private fun initListener() {
+        binding.etRule.signNameClear.setOnClickListener {
+            binding.etRule.fairerEt.setText(R.string.sign_name_blank)
+        }
+
+        binding.ruleHeader.apply {
+            defaultHeaderBackBtn.setOnClickListener {
+                it.findNavController().navigateUp()
+            }
+        }
+
+        binding.etRule.clFairerEt.setOnClickListener {
+            binding.etRule.fairerEt.requestFocus()
+            binding.etRule.fairerEt.isFocusable = true
+            binding.etRule.fairerEt.isCursorVisible = true
+        }
+
+        binding.etRule.fairerEt.apply {
+            setOnClickListener { it as EditText
+                if (!it.isCursorVisible) { it.isCursorVisible = true }
+            }
+            listenTextChanged()
+            listenEditorDoneAction(requireContext()) {
+                viewModel.createRule(it)
+            }
+        }
+    }
+
+
+    private fun EditText.listenTextChanged() {
+        this.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Timber.d("beforeTextChanged: ${p0.toString()}")
+                this@listenTextChanged.isCursorVisible = true
+                binding.isTextChanged = p0.toString().isNotEmpty()
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                //Timber.d("onTextChanged: ${p0.toString()}")
+                binding.isError = !p0.toString().matches(textPattern.toRegex())
+
+                if (viewModel.rules.value.count() >= 10) {
+                    this@listenTextChanged.text.clear()
+                    hideKeyboard(requireContext(), this@listenTextChanged)
+                    this@listenTextChanged.isCursorVisible = false
+                    binding.isError = true
+                    binding.ivInfo.setColorFilter(requireContext().getColor(R.color.negative_20))
+                    Toast.makeText(requireContext(), R.string.rule_info, Toast.LENGTH_LONG).show()
+                } else {
+                    binding.ivInfo.setColorFilter(requireContext().getColor(R.color.gray_600))
+                }
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                //Timber.d("afterTextChanged: ${p0.toString()}")
+                binding.isTextChanged = p0.toString().isNotEmpty()
+            }
+
+        })
+    }
+
+
 }
---
+++ app/src/main/java/com/depromeet/housekeeper/util/EditTextUtil.kt
@@ -0,0 +1,34 @@
+package com.depromeet.housekeeper.util
+
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import timber.log.Timber
+
+object EditTextUtil {
+    const val textPattern = "[0-9|a-z|A-Z|ㄱ-ㅎ|ㅏ-ㅣ|가-힝|ㆍᆢ| ]*"
+
+    fun hideKeyboard(context: Context, view: View) {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    fun EditText.listenEditorDoneAction(context: Context, onDone: (text: String) -> Unit) {
+        var handled = false
+        this.setOnEditorActionListener { textView, actionId, keyEvent ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                Timber.d("setOnEditorActionListener: ${textView.text}, $actionId, $keyEvent")
+                onDone(textView.text.toString())
+                this.text.clear()
+                hideKeyboard(context, this)
+                this.isCursorVisible = false
+                handled = true
+            }
+            handled
+        }
+        if (handled) return
+    }
+}
