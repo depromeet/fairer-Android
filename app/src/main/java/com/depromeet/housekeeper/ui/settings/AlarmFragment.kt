6530c1e29ce58cb3f579beabc3b7c8412e1d8764
sujin-kk <sjoo0002@gmail.com>
2022-08-24 01:53:42 +0900

    feat : create alarm view

---
+++ app/src/main/java/com/depromeet/housekeeper/ui/settings/AlarmFragment.kt
@@ -0,0 +1,37 @@
+package com.depromeet.housekeeper.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.depromeet.housekeeper.R
+import com.depromeet.housekeeper.databinding.FragmentAlarmBinding
+
+
+class AlarmFragment : Fragment() {
+
+    lateinit var binding: FragmentAlarmBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_alarm, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.alarmHeader.apply {
+
+        }
+    }
+
+}
