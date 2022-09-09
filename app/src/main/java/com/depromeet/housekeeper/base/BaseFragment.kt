9204f77ea5ae51d022ced50d2a791a118762afc0
kimminju99 <mindori1019@gmail.com>
2022-09-10 02:09:10 +0900

    Refactor/base: BaseFragment.kt 생성

---
+++ app/src/main/java/com/depromeet/housekeeper/base/BaseFragment.kt
@@ -0,0 +1,38 @@
+package com.depromeet.housekeeper.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B: ViewDataBinding>(private val resId: Int): Fragment() {
+
+    private var _binding: B? = null
+    val binding get() = _binding!!
+
+    abstract fun createView(binding: B)
+    abstract fun viewCreated() // livedata observing, adapter setting 등의 작업 진행
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, resId, container, false)
+        createView(binding)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewCreated()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
