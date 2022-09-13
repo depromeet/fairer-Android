132fe01f50f51f55d5b103b1d4b4c952909279ff
wjdwns <wjdwns3744@naver.com>
2022-09-13 21:56:01 +0900

    adjust Repository

--- app/src/main/java/com/depromeet/housekeeper/data/repository/MainRepository.kt
+++ app/src/main/java/com/depromeet/housekeeper/data/repository/MainRepository.kt
@@ -2,10 +2,66 @@ package com.depromeet.housekeeper.data.repository
 
 
 import com.depromeet.housekeeper.data.dataSource.RemoteDataSourceImpl
+import com.depromeet.housekeeper.model.HouseWork
+import com.depromeet.housekeeper.model.request.*
+import com.depromeet.housekeeper.model.response.*
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class MainRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSourceImpl
 ) {
+    /**
+     * houseWorks
+     */
+    suspend fun createHouseWorks(houseWorks: Chores): Flow<HouseWorkCreateResponse> =
+        remoteDataSource.createHouseWorks(houseWorks)
+
+    suspend fun editHouseWork(houseWorkId: Int, chore: Chore): Flow<HouseWork> =
+        remoteDataSource.editHouseWork(houseWorkId,chore)
+
+    suspend fun deleteHouseWork(houseWorkId: Int): Flow<Unit> =
+        remoteDataSource.deleteHouseWork(houseWorkId)
+
+    suspend fun updateChoreState(
+        houseWorkId: Int,
+        updateChoreBody: UpdateChoreBody,
+    ): Flow<UpdateChoreResponse> = remoteDataSource.updateChoreState(houseWorkId,updateChoreBody)
+
+    suspend fun getDetailHouseWorks(houseWorkId: Int): Flow<HouseWork> =
+        remoteDataSource.getDetailHouseWorks(houseWorkId)
+
+    suspend fun getDateHouseWorkList(
+        fromDate: String,
+        toDate: String
+    ): Flow<Map<String, HouseWorks>> = remoteDataSource.getDateHouseWorkList(fromDate, toDate)
+
+    suspend fun getPeriodHouseWorkListOfMember(
+        teamMemberId: Int,
+        fromDate: String,
+        toDate: String
+    ): Flow<Map<String, HouseWorks>> = remoteDataSource.getPeriodHouseWorkListOfMember(teamMemberId, fromDate, toDate)
+
+    suspend fun getCompletedHouseWorkNumber(scheduledDate: String): Flow<CompleteHouseWork> =
+        remoteDataSource.getCompletedHouseWorkNumber(scheduledDate)
+
+    suspend fun getList(scheduledDate: String): Flow<List<HouseWorks>> =
+        remoteDataSource.getList(scheduledDate)
+
+    /**
+     * presets
+     */
+    suspend fun getHouseWorkList(): Flow<List<ChoreList>> =
+        remoteDataSource.getHouseWorkList()
+
+    /**
+     * rules
+     */
+    suspend fun createRule(rule: Rule): Flow<RuleResponses> =
+        remoteDataSource.createRule(rule)
+    suspend fun getRules(): Flow<RuleResponses> =
+        remoteDataSource.getRules()
+    suspend fun deleteRule(ruleId: Int): Flow<Response> =
+        remoteDataSource.deleteRule(ruleId)
 
 }
--- app/src/main/java/com/depromeet/housekeeper/data/repository/UserRepository.kt
+++ app/src/main/java/com/depromeet/housekeeper/data/repository/UserRepository.kt
@@ -2,19 +2,60 @@ package com.depromeet.housekeeper.data.repository
 
 
 import com.depromeet.housekeeper.data.dataSource.RemoteDataSourceImpl
-import com.depromeet.housekeeper.model.request.SocialType
-import com.depromeet.housekeeper.model.response.LoginResponse
+import com.depromeet.housekeeper.model.request.*
+import com.depromeet.housekeeper.model.response.*
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class UserRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSourceImpl
 ) {
-
+    /**
+     * oauth
+     */
     suspend fun getGoogleLogin(socialType: SocialType): Flow<LoginResponse> =
         remoteDataSource.getGoogleLogin(socialType)
 
     suspend fun logout(): Flow<Unit> = remoteDataSource.logout()
 
+    /**
+     * teams
+     */
+    suspend fun buildTeam(buildTeam: BuildTeam) : Flow<BuildTeamResponse> =
+        remoteDataSource.buildTeam(buildTeam)
+
+    suspend fun getTeam(): Flow<Groups> = remoteDataSource.getTeam()
+
+    suspend fun getInviteCode(): Flow<GetInviteCode> = remoteDataSource.getInviteCode()
+
+    suspend fun updateTeam(teamName: BuildTeam): Flow<TeamUpdateResponse> =
+        remoteDataSource.updateTeam(teamName)
+
+    suspend fun joinTeam(inviteCode: JoinTeam): Flow<JoinTeamResponse> =
+        remoteDataSource.joinTeam(inviteCode)
+
+    suspend fun leaveTeam(): Flow<Unit> = remoteDataSource.leaveTeam()
+    /**
+     * members
+     */
+    suspend fun getProfileImages(): Flow<ProfileImages> =
+        remoteDataSource.getProfileImages()
+
+    suspend fun updateMember(updateMember: UpdateMember): Flow<UpdateMemberResponse> =
+        remoteDataSource.updateMember(updateMember)
+
+    suspend fun getMe(): Flow<ProfileData> = remoteDataSource.getMe()
+
+    suspend fun updateMe(editProfileModel: EditProfileModel): Flow<EditResponseBody> =
+        remoteDataSource.updateMe(editProfileModel)
+
+    /**
+    fcm
+     */
+    suspend fun saveToken(token: Token): Flow<Unit> =
+        remoteDataSource.saveToken(token)
+
+    suspend fun sendMessage(message: Message): Flow<Message> =
+        remoteDataSource.sendMessage(message)
 
 }
