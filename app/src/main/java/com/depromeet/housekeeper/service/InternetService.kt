bf79d71fecfc73841e5ddf9ae436661bf9107681
kimminju99 <mindori1019@gmail.com>
2022-09-29 17:51:16 +0900

    Feature/network : 인터넷 변화 감지하면 activity에 보내는 코드 구현 중

--- app/src/main/java/com/depromeet/housekeeper/service/InternetService.kt
+++ app/src/main/java/com/depromeet/housekeeper/service/InternetService.kt
@@ -3,17 +3,21 @@ package com.depromeet.housekeeper.service
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.Intent.ACTION_SEND
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
 import android.os.Binder
 import android.os.IBinder
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.depromeet.housekeeper.util.FILTER_NETWORK_CONNECTED
+import com.depromeet.housekeeper.util.IS_INTERNET_CONNECTED
 import timber.log.Timber
 
 class InternetService : Service() {
-    private var binder: IBinder = InternetBinder()
-    private lateinit var networkCallback: CheckNetworkConnected
+    private var binder: IBinder? = InternetBinder()
+    private var networkCallback: CheckNetworkConnected? = null
 
     inner class InternetBinder : Binder() {
         val service: InternetService
@@ -23,8 +27,16 @@ class InternetService : Service() {
     override fun onCreate() {
         super.onCreate()
         Timber.d(">>>>>>>> InternetService onCreate")
-        networkCallback = CheckNetworkConnected(applicationContext)
-        networkCallback.register()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        Timber.d(">>>>>>>> InternetService onStartCommand")
+        if (networkCallback == null) {
+            networkCallback = CheckNetworkConnected(applicationContext)
+            networkCallback!!.register()
+        }
+        return START_REDELIVER_INTENT
     }
 
     override fun onBind(p0: Intent?): IBinder? {
@@ -33,45 +45,61 @@ class InternetService : Service() {
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
+        super.onUnbind(intent)
         Timber.d(">>>>>>>> InternetService onUnbind")
-        return super.onUnbind(intent)
+        binder = null
+        return true
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        binder = null
+        networkCallback?.unregister()
         Timber.d(">>>>>>>> InternetService onDestroy")
     }
 
-    class CheckNetworkConnected(context: Context) : ConnectivityManager.NetworkCallback() {
+    class CheckNetworkConnected(val context: Context) : ConnectivityManager.NetworkCallback() {
         private val networkRequest: NetworkRequest = NetworkRequest.Builder()
             .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
             .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
             .build()
         private val connectivityManager: ConnectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        private var isNetworkConnected = false
 
         fun register() {
             connectivityManager.registerNetworkCallback(networkRequest, this)
             Timber.d(">>> CheckNetwork registered")
         }
 
+        fun unregister() {
+            connectivityManager.unregisterNetworkCallback(this)
+            Timber.d(">>> CheckNetwork unregistered")
+        }
+
+        private fun sendInternetConnected(value: Boolean) {
+            val intent = Intent(FILTER_NETWORK_CONNECTED).apply {
+                this.action = ACTION_SEND
+                this.putExtra(IS_INTERNET_CONNECTED, value)
+            }
+            LocalBroadcastManager.getInstance(context).sendBroadcast(intent)
+        }
+
         override fun onAvailable(network: Network) {
             super.onAvailable(network)
             Timber.d(">>> CheckNetwork onAvailable")
-            isNetworkConnected = true
+            sendInternetConnected(true)
         }
 
         override fun onLost(network: Network) {
             super.onLost(network)
             Timber.d(">>> CheckNetwork onLost")
-            isNetworkConnected = false
+            sendInternetConnected(false)
         }
 
         override fun onUnavailable() {
             super.onUnavailable()
             Timber.d(">>> CheckNetwork onUnavailable")
-            isNetworkConnected = false
+            sendInternetConnected(false)
         }
     }
 }
--- app/src/main/java/com/depromeet/housekeeper/ui/HouseKeeperActivity.kt
+++ app/src/main/java/com/depromeet/housekeeper/ui/HouseKeeperActivity.kt
@@ -1,20 +1,22 @@
 package com.depromeet.housekeeper.ui
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
+import android.content.*
+import android.content.Intent.ACTION_SEND
 import android.net.Uri
 import android.os.Bundle
 import android.os.IBinder
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.fragment.NavHostFragment
 import com.depromeet.housekeeper.R
 import com.depromeet.housekeeper.databinding.ActivityHousekeeperBinding
 import com.depromeet.housekeeper.service.InternetService
+import com.depromeet.housekeeper.util.FILTER_NETWORK_CONNECTED
+import com.depromeet.housekeeper.util.IS_INTERNET_CONNECTED
 import com.google.firebase.dynamiclinks.ktx.dynamicLinks
 import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,15 +27,25 @@ class HouseKeeperActivity : AppCompatActivity() {
 
     private val viewModel: HouseKeeperViewModel by viewModels()
     lateinit var binding: ActivityHousekeeperBinding
-    private val internetServiceConnection = object: ServiceConnection{
+    private val internetServiceConnection = object : ServiceConnection {
         override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
-            val internetService = service as InternetService.InternetBinder
+            LocalBroadcastManager.getInstance(applicationContext)
+                .registerReceiver(internetReceiver, IntentFilter(FILTER_NETWORK_CONNECTED)
+            )
         }
 
         override fun onServiceDisconnected(p0: ComponentName?) {
 
         }
+    }
 
+    private val internetReceiver = object : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, mIntent: Intent?) {
+            if (mIntent != null && mIntent.action == ACTION_SEND) {
+                val isInternetConnected = mIntent.getBooleanExtra(IS_INTERNET_CONNECTED, false)
+                Timber.d(">>> isInternetConnected = $isInternetConnected")
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +53,12 @@ class HouseKeeperActivity : AppCompatActivity() {
             setKeepOnScreenCondition { viewModel.isLoading.value }
         }
         super.onCreate(savedInstanceState)
-        applicationContext.bindService(Intent(this, InternetService::class.java), internetServiceConnection, Context.BIND_AUTO_CREATE)
+        applicationContext.startService(Intent(this, InternetService::class.java))
+//        applicationContext.bindService(
+//            Intent(this, InternetService::class.java),
+//            internetServiceConnection,
+//            Context.BIND_AUTO_CREATE
+//        )
         binding = DataBindingUtil.setContentView(this, R.layout.activity_housekeeper)
         binding.lifecycleOwner = this
         getDynamicLink()
