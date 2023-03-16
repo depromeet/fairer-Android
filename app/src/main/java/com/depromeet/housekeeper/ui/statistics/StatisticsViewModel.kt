cd7c242be4f9fac845a878c663406cd224561415
Minju Kim <mindori1019@gmail.com>
2023-03-16 22:42:36 +0900

    Style/statistics: 통계 화면 ui 초석 작업

---
+++ app/src/main/java/com/depromeet/housekeeper/ui/statistics/StatisticsViewModel.kt
@@ -0,0 +1,9 @@
+package com.depromeet.housekeeper.ui.statistics
+
+import com.depromeet.housekeeper.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class StatisticsViewModel @Inject constructor() : BaseViewModel() {
+}
