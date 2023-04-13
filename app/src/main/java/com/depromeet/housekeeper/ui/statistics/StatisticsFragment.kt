d4a289874d5fc935edbf8a03e7aa3aab01fe47b7
Minju Kim <mindori1019@gmail.com>
2023-04-13 17:14:40 +0900

    Feature/statistics: 통계 화면을 위한 api 연결 함수 viewModel에 구현

--- app/src/main/java/com/depromeet/housekeeper/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/depromeet/housekeeper/ui/statistics/StatisticsFragment.kt
@@ -1,10 +1,13 @@
 package com.depromeet.housekeeper.ui.statistics
 
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.depromeet.housekeeper.R
 import com.depromeet.housekeeper.base.BaseFragment
 import com.depromeet.housekeeper.databinding.FragmentStatisticsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -18,15 +21,31 @@ class StatisticsFragment : BaseFragment<FragmentStatisticsBinding>(R.layout.frag
 
     override fun viewCreated() {
         initView()
+        bindingVm()
     }
 
     fun initView() {
-        val dateFormat = SimpleDateFormat("MM")
-        val currentMonth: String = dateFormat.format(Date())
+        val yearMonthFormat = SimpleDateFormat("yyyy-MM")
+        viewModel.getStatistics(yearMonthFormat.format(Date()))
+
+        val monthFormat = SimpleDateFormat("MM")
+        val currentMonth: String = monthFormat.format(Date())
         binding.tvMonthTitle.text =
             String.format(getString(R.string.statistics_month_title, currentMonth))
         binding.tvTitle.text =
             String.format(getString(R.string.statistics_title, "김민주")) //todo 이름 넣기
         binding.tvTotalChores.text = String.format(getString(R.string.statistics_total_chores, 16))
+    }
+
+    fun bindingVm(){
+        lifecycleScope.launchWhenStarted {
+            viewModel.statsList.collect{
+                // todo adapter list로 넣기
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.statsHouseWork.collectLatest {  }
+        }
     }
 }
--- app/src/main/java/com/depromeet/housekeeper/ui/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/depromeet/housekeeper/ui/statistics/StatisticsViewModel.kt
@@ -1,9 +1,50 @@
 package com.depromeet.housekeeper.ui.statistics
 
+import androidx.lifecycle.viewModelScope
 import com.depromeet.housekeeper.base.BaseViewModel
+import com.depromeet.housekeeper.data.repository.StatisticsRepository
+import com.depromeet.housekeeper.model.response.HouseWorkStatsStatus
+import com.depromeet.housekeeper.model.response.StatsStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class StatisticsViewModel @Inject constructor() : BaseViewModel() {
+class StatisticsViewModel @Inject constructor(
+    private val statsRepository: StatisticsRepository
+) : BaseViewModel() {
+
+    private val _statsList: MutableStateFlow<List<StatsStatus>> = MutableStateFlow(listOf())
+    val statsList: StateFlow<List<StatsStatus>> get() = _statsList
+
+    private val _statsHouseWork: MutableStateFlow<MutableMap<String, HouseWorkStatsStatus>> =
+        MutableStateFlow(mutableMapOf())
+    val statsHouseWork: StateFlow<Map<String, HouseWorkStatsStatus>> get() = _statsHouseWork
+
+    /**
+     * Network Communication
+     */
+    fun getStatistics(yearMonth: String) {
+        viewModelScope.launch {
+            statsRepository.getStatistics(yearMonth).collectLatest {
+                val result = receiveApiResult(it) ?: return@collectLatest
+
+                _statsList.value = result.statisticsList
+            }
+        }
+    }
+
+    fun getHouseWorkStatistics(houseWorkName: String, yearMonth: String) {
+        viewModelScope.launch {
+            statsRepository.getHoseWorkStatistics(houseWorkName, yearMonth).collectLatest {
+                val result = receiveApiResult(it) ?: return@collectLatest
+
+                //todo 데이터 받아와 처리
+                _statsHouseWork.value[houseWorkName] = result.houseWorkStatisticsList[0]
+            }
+        }
+    }
 }
