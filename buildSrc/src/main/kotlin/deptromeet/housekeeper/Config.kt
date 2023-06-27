6764fdbfc12b3901d087c89435243b95534ccf3d
wjdwns <wjdwns3744@naver.com>
2023-06-27 23:02:34 +0900

    버전 업데이트

--- buildSrc/src/main/kotlin/deptromeet/housekeeper/Config.kt
+++ buildSrc/src/main/kotlin/deptromeet/housekeeper/Config.kt
@@ -4,6 +4,6 @@ object Config {
   const val COMPILE_SDK = 31
   const val TARGET_SDK = 31
   const val MIN_SDK = 28
-  const val VERSION_CODE = 7
-  const val VERSION_NAME = "1.4.0"
+  const val VERSION_CODE = 8
+  const val VERSION_NAME = "1.5.0"
 }
