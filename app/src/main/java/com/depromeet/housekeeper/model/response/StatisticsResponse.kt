1589db5769b65467b4be0b0902741adc1068d170
Minju Kim <mindori1019@gmail.com>
2023-07-13 22:34:54 +0900

    Feature/statistics : 집안일 현황 count 반드시 내림차순으로 오게 Comparable 구현 fix #179

--- app/src/main/java/com/depromeet/housekeeper/model/response/StatisticsResponse.kt
+++ app/src/main/java/com/depromeet/housekeeper/model/response/StatisticsResponse.kt
@@ -5,6 +5,7 @@ import com.squareup.moshi.Json
 data class StatsListResponse(
     @Json(name = "statisticResponseDtos") val statisticsList: List<StatsStatus>
 )
+
 data class StatsStatus(
     val houseWorkCount: Int,
     val houseWorkName: String,
@@ -13,12 +14,29 @@ data class StatsStatus(
 data class HouseWorkStatsResponse(
     @Json(name = "houseWorkStatics") val houseWorkStatisticsList: List<HouseWorkStatsMember>
 )
+
 data class HouseWorkStatsMember(
     val houseWorkCount: Int,
     val member: Member,
-)
+): Comparable<HouseWorkStatsMember> {
+    override fun compareTo(other: HouseWorkStatsMember): Int =
+    when {
+        houseWorkCount < other.houseWorkCount -> 1
+        houseWorkCount > other.houseWorkCount -> -1
+        else -> 0
+    }
+}
+
 data class Member(
     val memberId: Int,
     val memberName: String,
     val profilePath: String = "",
-)
+) : Comparable<Member> {
+    override fun compareTo(other: Member) = when {
+        memberName < other.memberName -> 1
+        memberName > other.memberName -> -1
+        else -> 0
+    }
+
+
+}
