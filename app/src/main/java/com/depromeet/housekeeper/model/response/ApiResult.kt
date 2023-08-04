8da3e89fda1ddc35743a8e5c8cc0809a83f92329
Minju Kim <mindori1019@gmail.com>
2023-08-04 15:53:32 +0900

    Fix : http unauthorized이면 AuthAuthenticator에서 처리해줬기 때문에 해당 apiResult 받아도 UI에 error 보여주지 않게 처리함

--- app/src/main/java/com/depromeet/housekeeper/model/response/ApiResult.kt
+++ app/src/main/java/com/depromeet/housekeeper/model/response/ApiResult.kt
@@ -3,6 +3,7 @@ package com.depromeet.housekeeper.model.response
 import com.depromeet.housekeeper.util.NETWORK_ERROR
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import okhttp3.internal.http.HTTP_UNAUTHORIZED
 import retrofit2.HttpException
 import timber.log.Timber
 
@@ -17,7 +18,7 @@ fun <T> safeFlow(apiFunc: suspend () -> T): Flow<ApiResult<T>> = flow {
         emit(ApiResult.Success(apiFunc.invoke()))
     }
     catch (e: HttpException) {
-        if (e.code() == NETWORK_ERROR) {
+        if (e.code() == NETWORK_ERROR || e.code() == HTTP_UNAUTHORIZED) {
             // todo 여기서 apiResult Loading으로 만들고 로딩중 화면 보여주게하려면 모든 네트워크 통신을 하나의 함수(baseRequest)에서 만들어서 해야할 듯
             Timber.e("auth ${e.message}")
         }
