71b1c397c3c871069a848d3b6c335a38285ba2c1
wjdwns <wjdwns3744@naver.com>
2023-08-10 12:39:06 +0900

    버전 수정

--- buildSrc/src/main/kotlin/deptromeet/housekeeper/Config.kt
+++ buildSrc/src/main/kotlin/deptromeet/housekeeper/Config.kt
@@ -4,6 +4,6 @@ object Config {
   const val COMPILE_SDK = 31
   const val TARGET_SDK = 31
   const val MIN_SDK = 28
-  const val VERSION_CODE = 8
-  const val VERSION_NAME = "1.4.1"
+  const val VERSION_CODE = 9
+  const val VERSION_NAME = "1.4.2"
 }
